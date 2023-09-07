430242288a17bbe3575c16440539f754ee7b79ff
Patrik Engborg <patrik@engborg.nu>
2023-09-07 23:48:27 +0200

    Add start page

--- app.vue
+++ app.vue
@@ -1,9 +1,48 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="container my-10 max-w-4xl">
+    <header class="border-b pb-3 mb-6 grid grid-cols-3 items-end">
+      <figure class="flex items-end gap-2 font-sans-alt">
+        <img class="w-20" src="@/assets/logo.png" />
+        <h1
+          class="font-bold uppercase mb-2 tracking-tight text-2xl sr-only md:not-sr-only"
+        >
+          Dev app
+        </h1>
+      </figure>
+
+      <nav>
+        <ul class="flex justify-center uppercase gap-4 font-medium">
+          <li><a href="/">Home</a></li>
+          <li><a href="/customers">Customers</a></li>
+        </ul>
+      </nav>
+
+      <div class="flex gap-3 justify-end">
+        <img
+          class="w-10 h-10 rounded-full"
+          src="https://files.startdeliver.com/accid-aa909493a6679e232a361e2a501179b4/611d57c1d8af8f2671ed9c25fb38331b"
+          alt=""
+        />
+
+        <p class="leading-tight sr-only md:not-sr-only">
+          <strong>Trazan Apansson</strong><br />
+          Customer success hero
+        </p>
+      </div>
+    </header>
+
+    <main>
+      <NuxtPage />
+    </main>
+
+    <footer></footer>
   </div>
 </template>
 
 <script>
-
+import "@fontsource/ibm-plex-sans/400.css";
+import "@fontsource/ibm-plex-sans/500.css";
+import "@fontsource/ibm-plex-sans/700.css";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,65 @@
 <template>
-  <div>Hello</div>
+  <div>
+    <h2 class="page-headline">Home</h2>
+
+    <dl class="grid md:grid-cols-2 gap-4 md:gap-12 mb-10">
+      <div class="bg-gray-100 p-5 rounded" v-for="{ label, sum } in stats">
+        <dt class="label mb-2 border-b border-gray-300">{{ label }}</dt>
+        <dd class="text-5xl font-bold tracking-tight">{{ sum }}</dd>
+      </div>
+    </dl>
+
+    <h3 class="label mb-4 border-b pb-2">New ARR per month</h3>
+
+    <ul class="grid grid-cols-6 gap-8 items-end">
+      <li v-for="item in report.data" class="">
+        <div class="bg-blue-600 rounded-t-lg" :style="{ height: `${item.arr / 1000}px` }"></div>
+        <time class="uppercase font-medium opacity-30 text-sm">{{ formatMonth(item.month) }}</time>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script setup>
+const { data: report } = await useFetch('https://startdeliver-mock-api.glitch.me/report')
+
+function getSum(arr, key) {
+  return arr.reduce((acc, currentValue) => {
+    return acc + currentValue[key]
+  }, 0)
+}
+
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  notation: 'compact',
+  compactDisplay: 'short'
+})
+
+const numFormatter = new Intl.NumberFormat('sv-SE')
+
+const stats = computed(() => [
+  {
+    label: 'ARR',
+    sum: currencyFormatter.format(getSum(report.value.data, 'arr'))
+  },
+  { label: 'Seats', sum: numFormatter.format(getSum(report.value.data, 'seats')) }
+])
+
+function formatMonth(dateString) {
+  let [year, month] = dateString.split('-')
+  year = parseInt(year)
+  month = parseInt(month) - 1 // Subtract 1 from the month to match JavaScript's month numbering (0-11).
+
+  return new Date(year, month).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short'
+  })
+}
+</script>
+
+<style>
+.label {
+  @apply font-bold opacity-50 uppercase;
+}
+</style>
